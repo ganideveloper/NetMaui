53fd2b55861364f4c7027ab0c438adfc9b3652db
gani.developer <gani.developer@gmail.com>
2022-08-25 19:30:05 +0700

    Add project files.

---
+++ Notes/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Notes
+{
+   public partial class App : Application
+   {
+      public App()
+      {
+         InitializeComponent();
+
+         MainPage = new AppShell();
+         Routing.RegisterRoute(nameof(Views.NotePage), typeof(Views.NotePage));
+      }
+   }
+}
